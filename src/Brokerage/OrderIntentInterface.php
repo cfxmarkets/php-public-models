b3c08d2a91afd663234f042e77d97b8cba53071e
Kael Shipman <kael.shipman@cfxtrading.com>
2017-11-09 18:21:41 -0600

    Added missing methods to OrderIntentEntity

--- src/Brokerage/OrderIntentInterface.php
+++ src/Brokerage/OrderIntentInterface.php
@@ -11,6 +11,8 @@ interface OrderIntentInterface extends \CFX\JsonApi\ResourceInterface {
     public function getUser();
     public function getAsset();
     public function getAssetIntent();
+    public function getAssetOwner();
+    public function getBankAccount();
 
     // Setters
     public function setType($val);
@@ -21,6 +23,8 @@ interface OrderIntentInterface extends \CFX\JsonApi\ResourceInterface {
     public function setUser(UserInterface $user=null);
     public function setAsset(\CFX\Exchange\AssetInterface $asset=null);
     public function setAssetIntent(\CFX\Brokerage\AssetIntentInterface $asset=null);
+    public function setAssetOwner(LegalEntityInterface $owner = null);
+    public function setBankAccount(BankAccountInterface $bankAccount = null);
 }
 
 
