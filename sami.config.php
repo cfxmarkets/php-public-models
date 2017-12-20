92aabdb24889fa53fe996452a36eac5ffafab009
Kael Shipman <kael.shipman@cfxtrading.com>
2017-12-20 10:12:50 -0600

    Updated sami config to build docs for master branch

--- sami.config.php
+++ sami.config.php
@@ -14,7 +14,7 @@ $iterator = Finder::create()
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
 $versions = GitVersionCollection::create($dir)
     ->addFromTags('v1.2.4')
-    ->addFromTags('v1.3.4')
+    ->add('master', 'Current Release')
 ;
 
 $sami = new Sami($iterator, array(
