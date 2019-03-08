b1d70bbcf9723b4f32b459844378fa6ccba91730
Kael Shipman <kael.shipman@cfxtrading.com>
2019-03-08 10:54:29 -0600

    Added ContractDSLQuery

---
+++ src/Brokerage/ContractsDSLQuery.php
@@ -0,0 +1,102 @@
+<?php
+namespace CFX\Brokerage;
+
+class ContractsDSLQuery extends \CFX\Persistence\GenericDSLQuery
+{
+    protected static function getAcceptableFields() {
+        return array_merge(parent::getAcceptableFields(), [ "contractType", "audience", "effectiveDate" ]);
+    }
+
+    protected static function getComparisonOperators() {
+        return array_merge(parent::getComparisonOperators(), [ "&", "|" ]);
+    }
+
+    public function includes($name)
+    {
+        return
+            array_key_exists($name, $this->expressions) &&
+            in_array($this->expressions[$name]['operator'], [ '=', "&", "in", "like", ">", ">=", "<", "<=" ], true);
+    }
+
+    public function setContractType($operator, $val) {
+        $acceptableOperators = [ "=", "!=", "like", "not like", "in", "not in" ];
+        if (!in_array($operator, $acceptableOperators)) {
+            throw new \CFX\Persistence\BadQueryException("You cannot use the '$operator' operator with field 'contractType'. This field only accepts operators '".implode("', '", $acceptableOperators)."'");
+        }
+
+        $this->setExpressionValue('contractType', [
+            'field' => 'contractType',
+            'operator' => $operator,
+            'value' => $val
+        ]);
+        return $this;
+    }
+
+    public function unsetContractType() {
+        $this->setExpressionValue('contractType', null);
+        return $this;
+    }
+
+    public function getContractType() {
+        return $this->getExpressionValue('contractType');
+    }
+
+
+
+
+
+
+    public function setAudience($operator, $val) {
+        $acceptableOperators = [ "=", "!=", "&", "|", "<", ">", "<=", ">=" ];
+        if (!in_array($operator, $acceptableOperators)) {
+            throw new \CFX\Persistence\BadQueryException("You cannot use the '$operator' operator with field 'contractType'. This field only accepts operators '".implode("', '", $acceptableOperators)."'");
+        }
+
+        $this->setExpressionValue('audience', [
+            'field' => 'audience',
+            'operator' => $operator,
+            'value' => $val
+        ]);
+        return $this;
+    }
+
+    public function unsetAudience() {
+        $this->setExpressionValue('audience', null);
+        return $this;
+    }
+
+    public function getAudience() {
+        return $this->getExpressionValue('audience');
+    }
+
+
+
+
+
+
+
+    public function setEffectiveDate($operator, $val) {
+        $acceptableOperators = [ "=", "!=", "<", ">", "<=", ">=", "in", "not in", "between" ];
+        if (!in_array($operator, $acceptableOperators)) {
+            throw new \CFX\Persistence\BadQueryException("You cannot use the '$operator' operator with field 'contractType'. This field only accepts operators '".implode("', '", $acceptableOperators)."'");
+        }
+
+        $this->setExpressionValue('effectiveDate', [
+            'field' => 'effectiveDate',
+            'operator' => $operator,
+            'value' => $val
+        ]);
+        return $this;
+    }
+
+    public function unsetEffectiveDate() {
+        $this->setExpressionValue('effectiveDate', null);
+        return $this;
+    }
+
+    public function getEffectiveDate() {
+        return $this->getExpressionValue('effectiveDate');
+    }
+}
+
+
